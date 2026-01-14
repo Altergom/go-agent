369bf17d67dbbdfdced2f45488c83a4a61976a4e
zrj <kele3325@gmail.com>
2026-01-14 20:36:48 +0800

    feat:加入milvus基本设置

---
+++ rag/indexer.go
@@ -0,0 +1,20 @@
+package rag
+
+import (
+	"context"
+
+	"github.com/cloudwego/eino-ext/components/indexer/milvus"
+)
+
+var Indexer *milvus.Indexer
+
+func NewIndexer(ctx context.Context) (*milvus.Indexer, error) {
+	indexer, err := milvus.NewIndexer(ctx, &milvus.IndexerConfig{
+		Client:    Milvus,
+		Embedding: Embedding,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return indexer, nil
+}
